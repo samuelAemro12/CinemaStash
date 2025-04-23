41cacef62553dbb84c8bd8edc2265adcf84e084c
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-04-23 12:37:36 -0700

    updated with the timestamp for user data schema

--- models/User.js
+++ models/User.js
@@ -1,33 +1,24 @@
-// models/User.js
-import mongoose from 'mongoose';
-
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  }
-});
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    }
+  }, { timestamps: true }); 
+  
 
 const User = mongoose.model('User', userSchema);
 export default User;
