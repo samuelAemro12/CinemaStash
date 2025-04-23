ddff9e3b5e25fb9154cd389671b7e4f70085721a
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-04-23 12:29:11 -0700

    connect to mongodb atlas

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-}
-);
-
-app.listen(3000, () => {    
-  console.log('Server is running on port 3000');
+    res.send('Hello World!');
 });
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://samuelaemrowork12:6yAihjy0iKVaWLcK@cinemastashapi.1ecvscf.mongodb.net/CinemaStashAPI?retryWrites=true&w=majority&appName=CinemaStashAPI',)
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));     
+
+  app.listen(3000, () => {    
+    console.log('Server is running on port 3000');
+  });
