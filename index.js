e2129bcafe4d8aff512a549b572e457590f8283d
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-04-23 11:13:03 -0700

    succussfully running on localhost

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("Hello, world!");
-console.log("This is a test.");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+}
+);
+
+app.listen(3000, () => {    
+  console.log('Server is running on port 3000');
+});
