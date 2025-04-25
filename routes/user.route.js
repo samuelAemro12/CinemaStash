1d416c7781c46bb9eaa00b3392004e40f968a63a
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-04-25 06:03:03 -0700

    demo route callback function

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,3 +1,14 @@
 import express from 'express';
 import router from express.Router();
+import { User } from '../models/user.model.js';
 
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json(users);
+    } catch (error) {
+        console.error('Error retrieving users:', error);
+        res.status(500).json({Message: error.message});
+    }
+});
