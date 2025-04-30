2bdaa9dae272467d76c53ae1ea74d296769734e2
samuelAemro12 <samuelaemrowork12@gmail.com>
2025-04-30 20:02:13 +0300

    added the full atrirbutes of a movie in the schema

--- models/movie.model.js
+++ models/movie.model.js
@@ -1,8 +1,34 @@
 import mongoose from 'mongoose';
 
 const movieSchema = new mongoose.Schema({
-
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  releaseDate: {
+    type: Date,
+    required: true
+  },
+  genre: {
+    type: [String],
+    default: []
+  },
+  posterUrl: {
+    type: String,
+    default: ''
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Movie = mongoose.model('Movie', movieSchema);
+
 export default Movie;
